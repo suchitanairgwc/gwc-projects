3a5d11ed06dbea5ed1cd753cf66d1834c38efb3d
suchitanairgwc <suchita.nair@girlswhocode.com>
2017-07-09 21:16:00 -0700

    Add files via upload

---
+++ arduino/starter-code/servo/servomove.ino.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo servoLeft;
+Servo servoRight;
+
+void setup() {
+  // put your setup code here, to run once:
+  servoLeft.attach(13);
+  servoRight.attach(12);
+}
+
+void loop() {
+  servoLeft.writeMicroseconds(1700); //left wheel, robot is going clockwise
+  servoRight.writeMicroseconds(1500); //pause
+  delay(500);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1300); // right wheel, robot goes counter clockwise
+  
+}
