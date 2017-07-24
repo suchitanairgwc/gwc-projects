c2a4bfb00795b1d5fa21008907172016966850ea
suchitanairgwc <suchita.nair@girlswhocode.com>
2017-07-24 14:21:36 -0700

    Add files via upload

---
+++ python/starter-code/data_science/movies.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+# Using a specific plotting mechanism
+
+movieData = pd.read_csv('actors.csv',sep=',')
+
+# TODO , lookup these functions
+#* nlargest()
+#* head()
+
+# Add the rest of the code here to show the correct data.
